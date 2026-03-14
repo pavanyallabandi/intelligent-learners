04f015c6adbfabb178eac0019d383c7bc443c93e
pavanyallabandi <pavanto3d@gmail.com>
2026-03-14 11:26:11 +0530

    Fix Vercel build script python version command

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # Build Script for Vercel
 echo "Building project..."
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 echo "Running migrations..."
-python manage.py migrate
+python3 manage.py migrate
